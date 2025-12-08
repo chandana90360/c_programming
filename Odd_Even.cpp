0d5d2bdd937555872495b2cf1513e2783aa8ce23
chandana90360 <chandana.sm55@gmail.com>
2025-12-08 23:00:17 +0530

    Add files via upload

---
+++ Odd_Even.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int var = 101;
+    if (var % 2 == 0)
+    {
+        printf("Even number");
+    }
+    else
+    {
+        printf("Odd number");
+    }
+    return 0;
+}
+
