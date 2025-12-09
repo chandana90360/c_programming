b8e87a5aa39d18f2b3cde36cc512f1e8a58b2564
chandana90360 <chandana.sm55@gmail.com>
2025-12-09 07:23:56 +0530

    Add files via upload

---
+++ arthimatic.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int arg1 = 10;
+	int arg2 = 20;
+	printf("****Arthimatic operations****\n");
+	printf("(arg1+arg2): %d\n", (arg1 + arg2));
+	printf("(arg1-arg2): %d\n", (arg1 - arg2));
+	printf("(arg1*arg2): %d\n", (arg1 * arg2));
+	printf("(arg1/arg2): %f\n", ((float)arg1 / arg2));
+	printf("(arg1%arg2): %d\n", (arg1 % arg2));
+	return 0;
+}
