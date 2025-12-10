61ef01f2fb261b004c83b7be109e86f69e02b9d3
chandana90360 <chandana.sm55@gmail.com>
2025-12-10 07:09:14 +0530

    Add files via upload

---
+++ Relational_operator.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int arg1 = 10;
+    int arg2 = 20;
+    printf("(arg1 > arg2)? : %d\n", (arg1 > arg2));
+    printf("(arg1 >= arg2)? : %d\n", (arg1 >= arg2));
+    printf("(arg1 < arg2)? : %d\n", (arg1 < arg2));
+    printf("(arg1 <= arg2)? : %d\n", (arg1 <= arg2));
+    printf("(arg1 == arg2)? : %d\n", (arg1 == arg2));
+    printf("(arg1 != arg2)? : %d\n", (arg1 != arg2));
+    return 0;
+}
